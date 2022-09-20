9baad55b5600d6cae88a018bcdfc7eedb9313ecc
nataliealwin <817350@mystma.org>
2022-09-20 15:20:14 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x,y):
+    if x > y:
+        return "true"
+    else:
+        return "false"
+def lessThan(x,y):
+    if x < y:
+        return "true"
+    else:
+        return "false"
+def equalTo(x,y):
+    if x == y:
+        return "true"
+    else:
+        return "false"
+def greaterOrEqual(x,y):
+    if x >= y:
+        return "true"
+    else:
+        return "false"
+def lessOrEqual(x,y):
+    if x <= y:
+        return "true"
