99fd8e57bcb1ae9f6e82e3589a28a31399697241
nataliealwin <817350@mystma.org>
2022-09-21 14:16:07 -0500

    3

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ def greaterThan(x,y):
         return "true"
     else:
         return "false"
+
 def lessThan(x,y):
     if x < y:
         return "true"
     else:
         return "false"
+
 def equalTo(x,y):
     if x == y:
         return "true"
     else:
         return "false"
+
 def greaterOrEqual(x,y):
     if x >= y:
         return "true"
     else:
         return "false"
+        
 def lessOrEqual(x,y):
     if x <= y:
         return "true"
